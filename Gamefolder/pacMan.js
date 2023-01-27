cc9e8f11bec9d9c880d392f7a5151048c6e65ceb
Saesi <saebjornga20@tskoli.is>
2023-01-27 14:43:23 +0000

    pacman moving on pos

--- Gamefolder/pacMan.js
+++ Gamefolder/pacMan.js
@@ -4,10 +4,11 @@ let ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var posPacX = 200;
-var posPacY = 200;
+var posPacX = 900;
+var posPacY = 500;
 function drawPacMan(posX, posY) {
     ctx.beginPath();
+    //ctx.arc(posX, posY, 15, 0.2 * Math.PI, 1.8 * Math.PI);
     ctx.arc(posX, posY, 15, 0.2 * Math.PI, 1.8 * Math.PI);
     ctx.lineTo(posX, posY);
     ctx.fillStyle = "yellow";
@@ -23,5 +24,28 @@ function drawArenaBorder(lT, rB) {
     ctx.clearRect(lT+5, 55, 489, rB-10);
 }
 
-drawArenaBorder(borderLeftTop, borderRightBotton);
-drawPacMan(posPacX, posPacY);
+
+
+function animate() {
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
+    drawArenaBorder(borderLeftTop, borderRightBotton);
+    drawPacMan(posPacX, posPacY);
+    document.onkeydown = function(event) {
+        switch (event.keyCode) {
+        case 37:
+            posPacX -= 3;
+        break;
+        case 38:
+            posPacY -= 3;
+        break;
+        case 39:
+            posPacX += 3;
+        break;
+        case 40:
+            posPacY += 3;
+        break;
+        }
+    };
+}
+animate();
